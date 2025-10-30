c0baf9a4d2b4f8763df2959e40f00837d063641d
Rabi <samanthav56ist@gmail.com>
2025-10-30 14:18:05 -0700

    added new program
    deleted old program that had gradle and added new program that has maven

--- Bookwise/src/main/java/org/example/bookwise/BookwiseApplication.java
+++ Bookwise/src/main/java/org/example/bookwise/BookwiseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookwiseApplication {
+public class BookWiseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookwiseApplication.class, args);
+		SpringApplication.run(BookWiseApplication.class, args);
 	}
 
 }
--- Bookwise/src/test/java/org/example/bookwise/BookwiseApplicationTests.java
+++ Bookwise/src/test/java/org/example/bookwise/BookwiseApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookwiseApplicationTests {
+class BookWiseApplicationTests {
 
 	@Test
 	void contextLoads() {
